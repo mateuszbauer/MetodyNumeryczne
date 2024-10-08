3b3be3f2030e4b57383d4d12dffe5b50dce3fe09
mateuszbauer <bauermateusz@gmail.com>
2024-10-08 17:12:52 +0200

    zadanie2 poprawione

--- laboratoria/zestaw_1/zadanie2/zadanie2.m
+++ laboratoria/zestaw_1/zadanie2/zadanie2.m
@@ -18,14 +18,19 @@ function A = my_polyfit(x, y, m)
     A = A'; 
 end
 
-% funkcja pomocnicza, oblicza wartosc f(x) dla wielomianu ze
+% funkcja pomocnicza, oblicza wartosc g(x) dla wielomianu ze
 % wspolczynnikami A
-function n = f(A, x)
+function n = g(A, x)
     y = 0;
     for i = 1:length(A)
         y = y + A(i) * x^(i-1);
     end
     n = y;
+end
+
+% f(x) = exp(-0.1x) * [g(x)]^2
+function n = f(A, x)
+    n = exp(-0.1 * x) * g(A, x)^2;
 end
 
 
@@ -47,49 +52,40 @@ function plot_results(A, x, y, color)
     x_plot = linspace(min(x)-3, max(x)+3, 1000);
     y_plot = zeros(1, length(x_plot));
     for i = 1:length(y_plot)
-        y_plot(i) = f(A, x_plot(i));
+        y_plot(i) = g(A, x_plot(i));
     end
     plot(x_plot, y_plot, Color=color, LineStyle='-');
     plot(x, y, 'Color', color, 'Marker', "o", 'LineStyle', 'none');
-
+    xlim([min(x)-1 max(x)+1]);
+    ylim([min(y)-1 max(y)+1])
 end
-
-
-degree = 4;
-A1 = my_polyfit(x, y, degree);
-A2 = polyfit(x, y, degree);
-
-figure;
-hold on;
-grid on;
-plot_results(A1, x, y, 'b');
-title('Moja aproksymacja');
-
-
-% polyfit zwraca wspolczynniki od najwiekszego na najmniejszego, trzeba
-% obrocic
-figure;
-grid on;
-hold on;
-plot_results(flip(A2), x, y, 'r');
-title('Polyfit');
-
-%n = calculate_integral(A2, -2, 2);
-
-
 
 degrees = [3 4 5 6 7 8 9];
 integral_polyfit = zeros(1, length(degrees));
 integral_my_polyfit = zeros(1, length(degrees));
+colors = ['b' 'r' 'g' 'c' 'm' 'y' 'k'];
 
+figure;
+grid on;
+hold on;
 for i = 1:length(degrees)
     d = degrees(i);
-    A1 = my_polyfit(x, y, d);
-    A2 = polyfit(x, y, d);
-
-    integral_my_polyfit(i) = calculate_integral(A1, -2, 2);
-    integral_polyfit(i) = calculate_integral(flip(A2), -2, 2);
+    A = my_polyfit(x, y, d);
+    plot_results(A, x, y, colors(i));
+    integral_my_polyfit(i) = calculate_integral(A, -2, 2);
 end
+title('Moja aproksymacja');
+
+figure;
+grid on;
+hold on;
+for i = 1:length(degrees)
+    d = degrees(i);
+    A = polyfit(x, y, d);
+    plot_results(flip(A), x, y, colors(i));
+    integral_polyfit(i) = calculate_integral(flip(A), -2, 2);
+end
+title('Polyfit');
 
 
 
