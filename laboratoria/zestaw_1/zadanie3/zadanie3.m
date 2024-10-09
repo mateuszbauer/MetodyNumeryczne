c800e1db732343607f07224b156b212e763a8ff8
mateuszbauer <bauermateusz@gmail.com>
2024-10-09 20:39:22 +0200

    zadanie 3 poprawione

--- laboratoria/zestaw_1/zadanie3/zadanie3.m
+++ laboratoria/zestaw_1/zadanie3/zadanie3.m
@@ -1,3 +1,20 @@
 
+C = 1.1 * 1e-3;
+R = 1 * 1e3;
+i = 1.2 * 1e-3;
+
+dC = 0.055 * 1e-3;
+dR = 0.02 * 1e3;
+di = 0.012 * 1e-3;
+
+function n = P(R, i, C)
+    n = (R^2 * i^2) / (C+R);
+end
+
+% Błąd względny
+dP = (R * i^2 * (2*C+R) * dR)/((C+R)^2) + (2 * R^2 * i * di)/(C+R) + (R^2 * i^2 * dC)/((C+R)^2)
+
+% Błąd bezwzględny
+dPw = dP / P(R, i , C)
 
 
