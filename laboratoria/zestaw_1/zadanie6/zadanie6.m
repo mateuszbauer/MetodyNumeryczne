a7041ac193acab73738b175a714088dc5177e366
mateuszbauer <bauermateusz@gmail.com>
2024-10-19 00:02:10 +0200

    zadanie 6

--- laboratoria/zestaw_1/zadanie6/zadanie6.m
+++ laboratoria/zestaw_1/zadanie6/zadanie6.m
@@ -1,3 +1,39 @@
 
 
+a = 0;
+b = 0.8;
+n = 4;
+x = linspace(a, b, n+1);
+h = (b-a)/n;
+alpha = 4.02;
+beta = -2.62;
+
+y = [-1.79 -2.26 -1.59];
+
+c = zeros(1, length(x));
+
+A = [4 2 0; 1 4 1; 0 2 4];
+
+
+c(2:4) = A \ [y(1)+(h*alpha)/3 y(2) y(3)-(h*beta)/3]';
+c(1) = c(3) - (h*alpha)/3;
+c(5) = c(3) + (h*beta)/3;
+
+
+function y = phi(x)
+    if x < 0
+        y = 0;
+    elseif x <= 0.2
+        y = x^3 / h;
+    elseif x <= 0.4
+        y = (x^3 - 4*(x-0.2)^3)/h;
+    elseif x <= 0.6
+        y = ((0.8-x)^3 - 4*(0.6-x)^3)/h;
+    elseif x <= 0.8
+        y = (0.8-x)^3;
+    else
+        y = 0;           
+end
+
+
 
